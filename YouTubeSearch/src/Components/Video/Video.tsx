744bab21e671cb5f561915efff7f217b849ca908
Nickerm <70840188+NickErmol@users.noreply.github.com>
2022-06-09 00:14:38 +0400

    update video.tsx

--- YouTubeSearch/src/Components/Video/Video.tsx
+++ YouTubeSearch/src/Components/Video/Video.tsx
@@ -9,7 +9,18 @@ import style from './video.module.css';
 import transformPublishedAtTime from '../../utils/transformPublishedAtTime';
 
 interface Props {
-  video: any
+  video:{
+    id: {videoId?: string} | string,
+    snippet: {
+            title: string,
+            publishedAt: string,
+            thumbnails: {
+              medium: {
+                url: string,
+              },
+            },
+          },
+        }
 }
 
 const Video: React.FC<Props> = ({ video }) => {
@@ -22,7 +33,7 @@ const Video: React.FC<Props> = ({ video }) => {
     },
   } = video;
   console.log(video);
-  const videoId = id?.videoId || id;
+  const videoId = id || id?.videoId;
 
   const navigate = useNavigate();
 
@@ -35,10 +46,9 @@ const Video: React.FC<Props> = ({ video }) => {
       <div>
         <LazyLoadImage src={medium.url} effect="blur" />
       </div>
-      <div className={style.discription}>
+      <div className={style.description}>
         <p>{title}</p>
         <div>
-          {/* <span>{views} - Views </span> */}
           <span> Published - {transformPublishedAtTime(publishedAt)}</span>
         </div>
       </div>
@@ -47,21 +57,3 @@ const Video: React.FC<Props> = ({ video }) => {
 };
 
 export default Video;
-
-// Video.propTypes = {
-//   video: PropTypes.shape({
-//     id: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-//     snippet: PropTypes.shape({
-//       title: PropTypes.string,
-//       publishedAt: PropTypes.string,
-//       thumbnails: PropTypes.shape({
-//         medium: PropTypes.shape({
-//           url: PropTypes.string,
-//         }),
-//       }),
-//     }),
-//     statistics: PropTypes.shape({
-//       viewCount: PropTypes.string,
-//     }),
-//   }).isRequired,
-// };
